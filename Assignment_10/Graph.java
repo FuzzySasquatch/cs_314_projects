14df782667d1dc249528dac93f78d08c154478a4
Phoenix Bishea <phoenixb@comcast.net>
2014-11-22 22:00:59 -0600

    Add A* algorithm.

--- Assignment_10/Graph.java
+++ Assignment_10/Graph.java
@@ -130,30 +130,29 @@ public double gcdist (double lata, double longa, double latb, double longb)
     // ****** your code starts here ******
 
 public int dijkstra( Vertex s ) {
-  
+  int cost = 0;
   for ( Vertex v : vertices.values() ) {
     v.visited = false;
     v.cost = 999999; }
   s.cost = 0;
   s.parent = null;
   PriorityQueue<Vertex>
-      fringe = new PriorityQueue<Vertex>(20,
-        new Comparator<Vertex>() {
-          public int compare(Vertex i, Vertex j) {
-              return (i.cost - j.cost); }});
+    fringe = new PriorityQueue<Vertex>(20,
+      new Comparator<Vertex>() {
+        public int compare(Vertex i, Vertex j) {
+          return (i.cost - j.cost); }});
   fringe.add(s);
-  int removeCount = 0;
   while ( ! fringe.isEmpty() ) {
     Vertex v = fringe.remove();  // lowest-cost
-    removeCount++;
+    cost++;
     if ( ! v.visited ) {
       v.visited = true;
       for ( Edge e : v.edges ) {
         int newcost = v.cost + e.cost;
-          if ( newcost < e.target.cost ) {
-            e.target.cost = newcost;
-            e.target.parent = v;
-            fringe.add(e.target);  } } } } return removeCount; }
+        if ( newcost < e.target.cost ) {
+          e.target.cost = newcost;
+          e.target.parent = v;
+          fringe.add(e.target); }}}} return cost; }
 
  // Prim finds the minimum spanning tree, i.e. the way to connect all nodes using minimum total cost.  
  // The total cost is the cost of all roads used in the Prim solution.  
@@ -184,15 +183,40 @@ public int prim( Vertex s ) {
   int cost = 0;
   for ( Vertex v : vertices.values() )
     cost += v.cost();
-  return cost; 
-}
+  return cost; }
 
-// public int astar( Vertex start, Vertex goal, Heuristic h ) {
-//  }
+public int astar( Vertex start, Vertex goal, Heuristic h ) {
+  int cost = 0;
+  for ( Vertex v : vertices.values() ) {
+    v.visited = false;
+    v.cost = 999999; }
+  start.cost = 0;
+  start.parent = null;
+  start.estimate = h.fn(start, goal); 
+  PriorityQueue<Vertex> 
+    fringe = new PriorityQueue<Vertex>(20,
+        new Comparator<Vertex>() {
+          public int compare(Vertex i, Vertex j) {
+                return ((i.cost + i.estimate) - (j.cost + j.estimate)); }});
+  fringe.add(start);
+  while ( ! fringe.isEmpty() ) {
+    Vertex v = fringe.remove();  // lowest-cost
+    cost++;
+    if (v.equals(goal))
+        return cost;
+    if ( ! v.visited ) {
+      v.visited = true;
+      for ( Edge e : v.edges ) {
+        int newcost = v.cost + e.cost;
+        if ( newcost < e.target.cost ) {
+          e.target.cost = newcost;
+          e.target.parent = v;
+          e.target.estimate = h.fn(e.target, goal);
+          fringe.add(e.target); }}}} return cost; }
 
-// public static final Heuristic dist = new Heuristic() {
-//         public Integer fn(Vertex from, Vertex to) {
-//             return from.distanceTo(to); } };
+public static final Heuristic dist = new Heuristic() {
+        public Integer fn(Vertex from, Vertex to) {
+            return from.distanceTo(to); } };
 
 // public static final Heuristic halfass = new Heuristic() {
 //  };
@@ -200,14 +224,14 @@ public int prim( Vertex s ) {
 // public static final Heuristic zip = new Heuristic() {
 //  };
 
-//     static Random rng = new Random();
+    static Random rng = new Random();
 
 // public static final Heuristic randombs = new Heuristic() {
 //  };
 
-// public static final Heuristic randomlies = new Heuristic() {
-//         public Integer fn(Vertex from, Vertex to) {
-//             return (Integer)((int) (rng.nextDouble() * 5000.0)); } };
+public static final Heuristic randomlies = new Heuristic() {
+        public Integer fn(Vertex from, Vertex to) {
+            return (Integer)((int) (rng.nextDouble() * 5000.0)); } };
 
 // returns the cost of an edge from start to goal
 // assuming direct edge between the two vertices
